9651b6165d52e1f171ea01f9c1bc02f0ee271209
Millicent Auma Omondi <162570349+millicentaumaomondi@users.noreply.github.com>
2024-10-12 14:21:07 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ with st.sidebar:
           'flipper_depth_mm':flipper_depth_mm,
           'body_mass_g':body_mass_g,
           'sex':sex}
-  input_df = pd.DataFrame(data,index[0])
+  input_df = pd.DataFrame(data,index=[0])
   input_penguins = pd.concat([input_df,X],axis=[0])
 
 input_penguins
