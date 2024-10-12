e13c0f7c11021b2cfcc272a5e98ebe0be0aa6bd3
Millicent Auma Omondi <162570349+millicentaumaomondi@users.noreply.github.com>
2024-10-12 13:22:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,10 @@ st.write("**Raw Data**")
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
 df 
 
+st.write("**X**")
+X = df.drop('species',axis=1)
+X
+
+st.write("**y**")
+y = df.species
+y
