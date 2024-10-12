9f0dc26bf402dc09e5c72495a4772fd22b9edacb
Millicent Auma Omondi <162570349+millicentaumaomondi@users.noreply.github.com>
2024-10-12 13:32:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,18 @@ import pandas as pd
 st.title('Machine Learning App 💻')
 
 st.info('This is a machine learning app, it builds machine learning models')
-st.write("**Raw Data**")
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-df 
+with st.expander("Data"):
+  st.write("**Raw Data**")
+  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  df 
 
-st.write("**X**")
-X = df.drop('species',axis=1)
-X
-
-st.write("**y**")
-y = df.species
-y
+  st.write("**X**")
+  X = df.drop('species',axis=1)
+  X
+  
+  st.write("**y**")
+  y = df.species
+  y
 
 with st.expander("Data Visualisation"):
   st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g",color='species')
