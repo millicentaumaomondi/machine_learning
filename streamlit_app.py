1b7d1a15956862dd5a9ef374ea6830188066c1e2
Millicent Auma Omondi <162570349+millicentaumaomondi@users.noreply.github.com>
2024-10-12 13:29:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ st.write("**y**")
 y = df.species
 y
 
-with st.expander("Data Visualisation")
-st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g",color='species')
+with st.expander("Data Visualisation"):
+  st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g",color='species')
 
