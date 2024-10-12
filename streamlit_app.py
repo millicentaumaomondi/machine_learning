29977ccd48e59ff7d6d5a535d651fd0257f673af
Millicent Auma Omondi <162570349+millicentaumaomondi@users.noreply.github.com>
2024-10-12 14:14:28 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,18 @@ with st.sidebar:
   bill_depth_mm = st.slider("Bill depth (mm)",13.1,21.5,17.2)
   flipper_depth_mm = st.slider("Flipper length (mm)",172.0,231.0,201.0)
   body_mass_g = st.slider("Body mass (mm)",2700.0,6300.0,4207.0)
-  gender = st.selectbox("Gender",("male", "female"))
+  sex = st.selectbox("Sex",("male", "female"))
   #create a data frame for input features
   data = {'island',island,
           'bill_length_mm',bill_length_mm,
           'bill_depth_mm',bill_depth_mm,
           'flipper_depth_mm',flipper_depth_mm,
           'body_mass_g',body_mass_g,
-          'gender',gender}
+          'sex',sex}
   input_df = pd.DataFrame(data,index[0])
+  input_penguins = pd.concat([input_df,X]axis=0)
+
+  input_penguins
           
           
           
